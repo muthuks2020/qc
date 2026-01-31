053ff984b97f51dccb6b7371d61273a26f89fdbb
muthuks <muthuks@gmail.com>
2026-01-31 15:00:08 +0530

    removed files

--- node_modules/src/components/dashboard/index.js
+++
@@ -1,6 +0,0 @@
-// Dashboard Components Exports
-export { StatCard } from './StatCard';
-export { JobCard } from './JobCard';
-
-export { default as StatCardDefault } from './StatCard';
-export { default as JobCardDefault } from './JobCard';
